1c0752c996122fdaf5020aa4bccae27c3bbf4cfe
Kadosh1978 <craving_2002@mail.ru>
2026-04-28 22:34:28 +0300

    Add Sergiev Posad content and route images

--- routes/admin.py
+++ routes/admin.py
@@ -1,10 +1,78 @@
 from django.contrib import admin
+
 from .models import Route
 
 
 @admin.register(Route)
 class RouteAdmin(admin.ModelAdmin):
-    list_display = ("title", "destination", "is_published", "created_at")
-    list_filter = ("is_published", "destination")
-    search_fields = ("title", "short_description", "content")
-    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "title",
+        "destination",
+        "estimated_time",
+        "is_published",
+        "created_at",
+    )
+    list_filter = (
+        "destination",
+        "is_published",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "slug",
+        "short_description",
+        "content",
+    )
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+    readonly_fields = (
+        "created_at",
+    )
+
+    fieldsets = (
+        (
+            "Основное",
+            {
+                "fields": (
+                    "destination",
+                    "title",
+                    "slug",
+                    "short_description",
+                    "estimated_time",
+                    "is_published",
+                )
+            },
+        ),
+        (
+            "Описание маршрута",
+            {
+                "fields": (
+                    "route_includes",
+                    "suitable_for",
+                    "content",
+                )
+            },
+        ),
+        (
+            "Фотографии маршрута",
+            {
+                "fields": (
+                    "route_image_1",
+                    "route_image_1_caption",
+                    "route_image_2",
+                    "route_image_2_caption",
+                    "route_image_3",
+                    "route_image_3_caption",
+                )
+            },
+        ),
+        (
+            "Служебное",
+            {
+                "fields": (
+                    "created_at",
+                )
+            },
+        ),
+    )
--- routes/models.py
+++ routes/models.py
@@ -10,6 +10,7 @@ class Route(models.Model):
         related_name="routes",
         verbose_name="Направление",
     )
+
     title = models.CharField("Название маршрута", max_length=200)
     slug = models.SlugField("Слаг", unique=True)
     short_description = models.CharField("Короткое описание", max_length=255)
@@ -19,12 +20,50 @@ class Route(models.Model):
         max_length=100,
         blank=True,
     )
+
     route_includes = models.TextField(
         "Что входит в маршрут",
         blank=True,
     )
+
     suitable_for = models.TextField(
         "Подходит кому",
+        blank=True,
+    )
+
+    route_image_1 = models.ImageField(
+        "Фото маршрута 1",
+        upload_to="routes/",
+        blank=True,
+        null=True,
+    )
+    route_image_1_caption = models.CharField(
+        "Подпись к фото маршрута 1",
+        max_length=255,
+        blank=True,
+    )
+
+    route_image_2 = models.ImageField(
+        "Фото маршрута 2",
+        upload_to="routes/",
+        blank=True,
+        null=True,
+    )
+    route_image_2_caption = models.CharField(
+        "Подпись к фото маршрута 2",
+        max_length=255,
+        blank=True,
+    )
+
+    route_image_3 = models.ImageField(
+        "Фото маршрута 3",
+        upload_to="routes/",
+        blank=True,
+        null=True,
+    )
+    route_image_3_caption = models.CharField(
+        "Подпись к фото маршрута 3",
+        max_length=255,
         blank=True,
     )
 
