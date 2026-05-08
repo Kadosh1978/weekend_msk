07b0c22596257b1cab417ee3997dfe98f5ccf2b9
Kadosh1978 <craving_2002@mail.ru>
2026-05-08 21:52:03 +0300

    0805

--- core/views.py
+++ core/views.py
@@ -4,14 +4,24 @@ from routes.models import Route
 
 
 def home(request):
-    destinations = Destination.objects.filter(is_published=True).order_by("name")[:6]
-    latest_routes = Route.objects.filter(is_published=True).select_related("destination").order_by("-created_at")[:6]
+    destinations_qs = Destination.objects.filter(is_published=True)
+
+    destinations = destinations_qs.order_by("name")[:6]
+    destinations_total = destinations_qs.count()
+
+    latest_routes = (
+        Route.objects
+        .filter(is_published=True)
+        .select_related("destination")
+        .order_by("-created_at")[:6]
+    )
 
     return render(
         request,
         "core/home.html",
         {
             "destinations": destinations,
+            "destinations_total": destinations_total,
             "latest_routes": latest_routes,
         },
     )
