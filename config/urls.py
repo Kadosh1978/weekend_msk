eabd7388c44748547a49bc8a9d8f4dcf039d6c8d
Kadosh1978 <craving_2002@mail.ru>
2026-05-14 06:35:46 +0300

    robo.txt

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from core.seo_views import robots_txt
+
+
 urlpatterns = [
+    path("robots.txt", robots_txt, name="robots_txt"),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("destinations/", include("destinations.urls")),
